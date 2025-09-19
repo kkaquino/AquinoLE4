65379f0202ab49cd66f61debacf7fd09449a2bed
kkaquino <kkdaquino@mymail.mapua.edu.ph>
2025-09-20 01:27:39 +0800

    Add files via upload

---
+++ BlogAPI/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using BlogDataLibrary.Models;
+using BlogDataLibrary.Data;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BlogAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly SqlData _db;
+
+        public LoginController(IConfiguration config, SqlData db)
+        {
+            _config = config;
+            _db = db;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public ActionResult Login([FromBody] UserLogin login)
+        {
+            var user = _db.Authenticate(login.UserName, login.Password).Result;
+            if (user != null)
+            {
+                var token = GenerateToken(user);
+                return Ok(new { Token = token });
+            }
+            return NotFound("User not found");
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public ActionResult Register([FromBody] UserModel user)
+        {
+            _db.Register(user.UserName, user.FirstName, user.LastName, user.Password);
+            return Ok("User registered.");
+        }
+
+        private string GenerateToken(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ BlogAPI/Controllers/PostController.cs
@@ -0,0 +1,50 @@
+﻿using BlogDataLibrary.Data;
+using BlogDataLibrary.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace BlogAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostController : ControllerBase
+    {
+        private readonly SqlData _db;
+
+        public PostController(SqlData db)
+        {
+            _db = db;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("list")]
+        public ActionResult<List<ListPostModel>> ListPosts()
+        {
+            var posts = _db.ListPosts();
+            return Ok(posts);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public ActionResult<ListPostModel> ShowPostDetails(int id)
+        {
+            var post = _db.ShowPostDetails(id);
+            return Ok(post);
+        }
+
+        private int GetCurrentUserId()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                string id = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+                if (id != null)
+                    return Convert.ToInt32(id);
+            }
+            return 0;
+        }
+    }
+}
