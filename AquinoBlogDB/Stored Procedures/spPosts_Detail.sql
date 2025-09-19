099311029e0867e11b7ecb43d627040e575173ab
kkaquino <kkdaquino@mymail.mapua.edu.ph>
2025-09-20 01:29:56 +0800

    Add files via upload

---
+++ AquinoBlogDB/Stored Procedures/spPosts_Detail.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spPosts_Detail]
+	@id int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT [p].[Id], [p].[Title], [p].[Body], [p].[DateCreated], 
+	[u].[UserName], [u].[FirstName], [u].[LastName]
+	FROM dbo.Posts p
+	INNER JOIN dbo.Users u
+	ON p.UserId = u.id
+	WHERE p.Id = @id;
+
+END
---
+++ AquinoBlogDB/Stored Procedures/spPosts_Insert.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spPosts_Insert]
+    @userId int,
+    @title nvarchar(50),
+    @body text,
+    @dateCreated datetime2
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO dbo.Posts
+        (UserId, Title, Body, DateCreated)
+    VALUES
+        (@userId, @title, @body, @dateCreated);
+END
